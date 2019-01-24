052a099b7945d224baee600126352a953aee0107
Katalin Csortos <katalin.csortos@gmail.com>
2019-01-24 14:56:05 +0100

    added formatted planet data

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    universe_data = requests.get('https://swapi.co/api/planets/?page=1').json()
+    planets = universe_data['results']
+    for planet in planets:
+        planet['diameter'] = format(int(planet['diameter']), ',d')
+        planet['population'] = format(int(planet['population']), ',d') \
+            if planet['population'] != 'unknown' else 'unknown'
+
+    return render_template('index.html', planets=planets)
 
 
 if __name__ == '__main__':
