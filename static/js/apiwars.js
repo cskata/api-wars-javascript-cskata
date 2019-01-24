8cea4733a732f6290c6d0ca926b3a5432bfdae89
Katalin Csortos <katalin.csortos@gmail.com>
2019-01-24 18:35:43 +0100

    added button for residents, now page is loading another 10 planets

--- static/js/apiwars.js
+++ static/js/apiwars.js
@@ -1,50 +1,70 @@
-let nextButton = document.getElementById('next-button');
-
-let whichPage = document.getElementById('planets').dataset.page;
-let targetURL = 'https://swapi.co/api/planets/?page=' + whichPage;
+init();
 
 
-$.ajax({
-    dataType: "json",
-    url: targetURL,
-    success: function (response) {
-        let table = document.getElementById('planets');
-        let planets = response['results'];
+function getData() {
+    let whichPage = document.getElementById('planets').dataset.page;
+    let targetURL = 'https://swapi.co/api/planets/?page=' + whichPage;
 
-        for (let planet of planets) {
-            planet['diameter'] = planet['diameter'].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    $.ajax({
+        dataType: "json",
+        url: targetURL,
+        success: function (response) {
+            let table = document.getElementById('planets');
+            let planets = response['results'];
 
-            if (planet['population'] !== 'unknown') {
-                planet['population'] = planet['population'].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            for (let planet of planets) {
+                planet['diameter'] = planet['diameter'].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+                if (planet['population'] !== 'unknown') {
+                    planet['population'] = planet['population'].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    planet['population'] = planet['population'] + ' people';
+                }
+
+                if (planet['surface_water'] !== 'unknown') {
+                    planet['surface_water'] = planet['surface_water'] + '%';
+                }
+
+
+                let row = table.insertRow(-1);
+
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
+                let cell6 = row.insertCell(5);
+                let cell7 = row.insertCell(6);
+                let cell8 = row.insertCell(7);
+
+                addClassToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8);
+                addDataToCells(cell1, cell2, cell3, cell4, cell5, cell6, planet);
+
+                if (planet['residents'].length === 0) {
+                    cell7.innerHTML = 'No known residents';
+                } else if (planet['residents'].length > 0) {
+                    let residentBtn = document.createElement('button');
+
+                    let noOfResidents = planet['residents'].length.toString();
+                    let fullResidentTxt = noOfResidents + ' resident(s)';
+                    let residentBtnTxt = document.createTextNode(fullResidentTxt);
+
+                    residentBtn.appendChild(residentBtnTxt);
+                    residentBtn.classList.add('btn');
+                    residentBtn.classList.add('btn-secondary');
+                    cell7.appendChild(residentBtn);
+                }
+
+                let voteBtn = document.createElement('button');
+                voteBtn.classList.add('btn');
+                voteBtn.classList.add('btn-secondary');
+
+                let text = document.createTextNode('Vote');
+                voteBtn.appendChild(text);
+                cell8.appendChild(voteBtn);
             }
-
-            if (planet['surface_water'] !== 'unknown') {
-                planet['surface_water'] = planet['surface_water'] + '%';
-            }
-
-
-            let row = table.insertRow(-1);
-
-            let cell1 = row.insertCell(0);
-            let cell2 = row.insertCell(1);
-            let cell3 = row.insertCell(2);
-            let cell4 = row.insertCell(3);
-            let cell5 = row.insertCell(4);
-            let cell6 = row.insertCell(5);
-            let cell7 = row.insertCell(6);
-            let cell8 = row.insertCell(7);
-
-            addClassToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8);
-            addDataToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, planet);
-
-
-            let voteBtn = document.createElement('button');
-            let text = document.createTextNode('Vote');
-            voteBtn.appendChild(text);
-            cell8.appendChild(voteBtn);
         }
-    }
-});
+    });
+}
 
 
 function addClassToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8) {
@@ -58,12 +78,29 @@ function addClassToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8)
     cell8.classList.add('planet-data');
 }
 
-function addDataToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, planet) {
+function addDataToCells(cell1, cell2, cell3, cell4, cell5, cell6, planet) {
     cell1.innerHTML = planet['name'];
     cell2.innerHTML = planet['diameter'];
     cell3.innerHTML = planet['climate'];
     cell4.innerHTML = planet['terrain'];
     cell5.innerHTML = planet['surface_water'];
     cell6.innerHTML = planet['population'];
-    cell7.innerHTML = planet['residents'].length;
+}
+
+
+function switchToNextPage() {
+    let currentPage = document.getElementById('planets');
+    console.log()
+    let currentPageNo = parseInt(document.getElementById('planets').dataset.page);
+    let nextPage = currentPageNo + 1;
+    currentPage.dataset.page = nextPage.toString();
+    getData();
+}
+
+
+function init() {
+    getData();
+
+    let nextButton = document.getElementById('next-button');
+    nextButton.addEventListener('click', switchToNextPage);
 }
