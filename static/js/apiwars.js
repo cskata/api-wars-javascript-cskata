4f23ecc5215e45026135e478fbc18383fa696a0b
Katalin Csortos <katalin.csortos@gmail.com>
2019-01-24 17:44:39 +0100

    planets' data is know formatted

--- static/js/apiwars.js
+++ static/js/apiwars.js
@@ -12,6 +12,17 @@ $.ajax({
         let planets = response['results'];
 
         for (let planet of planets) {
+            planet['diameter'] = planet['diameter'].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+            if (planet['population'] !== 'unknown') {
+                planet['population'] = planet['population'].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
+
+            if (planet['surface_water'] !== 'unknown') {
+                planet['surface_water'] = planet['surface_water'] + '%';
+            }
+
+
             let row = table.insertRow(-1);
 
             let cell1 = row.insertCell(0);
@@ -23,21 +34,36 @@ $.ajax({
             let cell7 = row.insertCell(6);
             let cell8 = row.insertCell(7);
 
-            cell1.innerHTML = planet['name'];
-            cell2.innerHTML = planet['diameter'];
-            cell3.innerHTML = planet['climate'];
-            cell4.innerHTML = planet['terrain'];
-            cell5.innerHTML = planet['surface_water'] + '%';
-            cell6.innerHTML = planet['population'];
-            cell7.innerHTML = planet['residents'].length;
+            addClassToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8);
+            addDataToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, planet);
 
-            let votebtn = document.createElement('button');
+
+            let voteBtn = document.createElement('button');
             let text = document.createTextNode('Vote');
-            votebtn.appendChild(text);
-            cell8.appendChild(votebtn);
+            voteBtn.appendChild(text);
+            cell8.appendChild(voteBtn);
         }
-
-
-        console.log(planets[0])
     }
 });
+
+
+function addClassToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8) {
+    cell1.classList.add('planet-data');
+    cell2.classList.add('planet-data');
+    cell3.classList.add('planet-data');
+    cell4.classList.add('planet-data');
+    cell5.classList.add('planet-data');
+    cell6.classList.add('planet-data');
+    cell7.classList.add('planet-data');
+    cell8.classList.add('planet-data');
+}
+
+function addDataToCells(cell1, cell2, cell3, cell4, cell5, cell6, cell7, planet) {
+    cell1.innerHTML = planet['name'];
+    cell2.innerHTML = planet['diameter'];
+    cell3.innerHTML = planet['climate'];
+    cell4.innerHTML = planet['terrain'];
+    cell5.innerHTML = planet['surface_water'];
+    cell6.innerHTML = planet['population'];
+    cell7.innerHTML = planet['residents'].length;
+}
