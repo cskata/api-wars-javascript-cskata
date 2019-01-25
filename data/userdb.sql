b6a252497cdf3226e3c7c3c5488cbfed7ba7246c
Katalin Csortos <katalin.csortos@gmail.com>
2019-01-25 18:19:43 +0100

    extended database with planet_votes table

--- data/userdb.sql
+++ data/userdb.sql
@@ -1,4 +1,8 @@
-﻿DROP TABLE IF EXISTS registered_users;
+﻿ALTER TABLE IF EXISTS ONLY public.registered_users DROP CONSTRAINT IF EXISTS registered_users_pk CASCADE;
+ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS planet_votes_pk CASCADE;
+ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+
+DROP TABLE IF EXISTS registered_users;
 DROP SEQUENCE IF EXISTS registered_users_id_seq;
 
 CREATE TABLE registered_users (
@@ -23,3 +27,31 @@ ALTER TABLE ONLY registered_users
 
 ALTER TABLE ONLY registered_users
     ADD CONSTRAINT username_uk UNIQUE (username);
+
+
+DROP TABLE IF EXISTS planet_votes;
+DROP SEQUENCE IF EXISTS planet_votes_id_seq;
+
+CREATE TABLE planet_votes (
+    id integer NOT NULL,
+    planet_id integer NOT NULL,
+    planet_name character varying(255) NOT NULL,
+    user_id integer NOT NULL,
+    submission_time timestamp without time zone
+);
+
+CREATE SEQUENCE planet_votes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE ONLY planet_votes ALTER COLUMN id SET DEFAULT nextval('planet_votes_id_seq'::regclass);
+
+ALTER TABLE ONLY planet_votes
+    ADD CONSTRAINT planet_votes_pk PRIMARY KEY (id);
+
+ALTER TABLE ONLY planet_votes
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES registered_users(id);
