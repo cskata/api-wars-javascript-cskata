53ec268b20c095f1753c9c0e1822e1c222256cad
Katalin Csortos <katalin.csortos@gmail.com>
2019-02-05 20:46:54 +0100

    refactor: removed creating table element from templates

--- static/js/templates.js
+++ static/js/templates.js
@@ -1,21 +1,4 @@
 export let templates = {
-    createTableElement: function () {
-        let planetData = document.createElement('div');
-        planetData.id = 'planet-data';
-        planetData.innerHTML =
-            `<table id="planets" data-page="1">
-                <th class="planet-header"></th>
-                <th class="planet-header"></th>
-                <th class="planet-header"></th>
-                <th class="planet-header"></th>
-                <th class="planet-header"></th>
-                <th class="planet-header"></th>
-                <th class="planet-header"></th>
-                <th class="planet-header"></th>
-            </table>`;
-
-        return planetData;
-    },
     createHeaderElement: function () {
         let header = document.createElement('tr');
         header.id = 'main-header';
