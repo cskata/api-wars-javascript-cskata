27d49364ec773457f47e5f1c04d5e6519c30456f
Katalin Csortos <katalin.csortos@gmail.com>
2019-02-06 01:05:13 +0100

    fix: simplified database

--- data/userdb.sql
+++ data/userdb.sql
@@ -1,26 +1,15 @@
-﻿ALTER TABLE IF EXISTS ONLY public.registered_users DROP CONSTRAINT IF EXISTS registered_users_pk CASCADE;
-ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS planet_votes_pk CASCADE;
+﻿ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS planet_votes_pk CASCADE;
 ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.registered_users DROP CONSTRAINT IF EXISTS registered_users_pk CASCADE;
 
 DROP TABLE IF EXISTS registered_users;
-DROP SEQUENCE IF EXISTS registered_users_id_seq;
 
 CREATE TABLE registered_users (
-    id integer NOT NULL,
+    id SERIAL,
     username character varying(255) NOT NULL,
     hashed_pw character varying(255) NOT NULL,
     reg_date timestamp without time zone
 );
-
-CREATE SEQUENCE registered_users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE ONLY registered_users ALTER COLUMN id SET DEFAULT nextval('registered_users_id_seq'::regclass);
 
 ALTER TABLE ONLY registered_users
     ADD CONSTRAINT registered_users_pk PRIMARY KEY (id);
@@ -30,25 +19,14 @@ ALTER TABLE ONLY registered_users
 
 
 DROP TABLE IF EXISTS planet_votes;
-DROP SEQUENCE IF EXISTS planet_votes_id_seq;
 
 CREATE TABLE planet_votes (
-    id integer NOT NULL,
+    id SERIAL,
     planet_id integer NOT NULL,
     planet_name character varying(255) NOT NULL,
     user_id integer NOT NULL,
     submission_time timestamp without time zone
 );
-
-CREATE SEQUENCE planet_votes_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE ONLY planet_votes ALTER COLUMN id SET DEFAULT nextval('planet_votes_id_seq'::regclass);
 
 ALTER TABLE ONLY planet_votes
     ADD CONSTRAINT planet_votes_pk PRIMARY KEY (id);
