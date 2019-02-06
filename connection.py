a5c92092ab24e0f46588a197378501da6fbcfda8
Katalin Csortos <katalin.csortos@gmail.com>
2019-02-06 02:34:56 +0100

    fix: trying to autocommit to heroku database

--- connection.py
+++ connection.py
@@ -35,6 +35,9 @@ def open_database():
             host=url.hostname,
             port=url.port
         )
+        # connection_string = get_connection_string()
+        # connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
         raise exception
